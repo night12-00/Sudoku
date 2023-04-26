81434281b4c70b1b7350dedc8c998bc7e8f3c124
Tan Hoang <78993276+Dominic-github@users.noreply.github.com>
2023-04-26 14:22:38 +0700

    test (#7)
    * Update Save
    
    * Test main

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include "include.h"
 
+void In(int n)
+{
+  while (n > 0)
+  {
+    cout << n << endl;
+    n--;
+  }
+}
+
 int main()
 {
-  while (true)
-  {
-    int key = getch();
-    cout << key << endl;
-  }
+  int n = 99;
+  In(n);
+
   cout << "Init Project";
   return 0;
 }
